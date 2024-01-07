561c68b803a2606fb4caf09305d058faa27cbb45
Takashi2377 <120803903+Takashi2377@users.noreply.github.com>
2024-01-07 14:58:20 +0800

    practice&comment

--- MainMission(week2)/login.js
+++ MainMission(week2)/login.js
@@ -1,1 +1,27 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
+createApp({
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      const api = "https://vue3-course-api.hexschool.io/v2/admin/signin"; //站點 + api文件中 "登入" 需要使用的路徑
+      axios
+        .post(api, this.user) //從文件中得知需使用post。 接收參數: 1.api路徑 2.依文件指示帶入所需物件
+        .then((res) => {
+          const { token, expired } = res.data; //從結果中，取出token & expired
+          document.cookie = `myToken=${token};expires=${new Date(expired)};`;
+          window.location = "products.html"; //帳密驗證成功，跳轉到產品頁面
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+  },
+}).mount("#app");
--- MainMission(week2)/products.js
+++ MainMission(week2)/products.js
@@ -1,1 +1,48 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
+createApp({
+  data() {
+    return {
+      url: "https://vue3-course-api.hexschool.io/v2",
+      path: "yuetin-hexschool",
+      products: [],
+      temp: {},
+    };
+  },
+  methods: {
+    checkAdmin() {
+      axios
+        .post(`${this.url}/api/user/check`)
+        .then(() => {
+          this.getData(); //確認驗證才能取得資料
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          window.location = "login.html"; //驗證失敗，跳轉回登入頁面
+        });
+    },
+    getData() {
+      axios
+        .get(`${this.url}/api/${this.path}/admin/products`) //帶入個人的 path 以便取得自己上傳的資料
+        .then((res) => {
+          this.products = res.data.products;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    openProduct(item) {
+      this.temp = item;
+    },
+  },
+  mounted() {
+    //取出token並夾帶到headers中做發送
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)myToken\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    axios.defaults.headers.common["Authorization"] = token;
+
+    this.checkAdmin();
+  },
+}).mount("#app");
